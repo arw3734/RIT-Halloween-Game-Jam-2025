3be93428d9f2a84ff2c8c77d877b3ca24e34f690
Aiden Weiskopff <aidenweiskopff@gmail.com>
2025-10-31 22:30:35 -0400

    deleted useless character body

--- roommate/roommate_wish_dir.gd
+++ roommate/roommate_wish_dir.gd
@@ -1,22 +1,15 @@
 extends WishDirection
-class_name RoommateWishDir
+class_name PlayerWishDir
 
-@export var nav_agent: NavigationAgent3D
 @export var current_position_node: Node3D;
-@export var target: Node3D;
 @export var distance_tolerance: float = 5;
+@export var sprite: Sprite3D;
 
 var direction_vec: Vector2 = Vector2.ZERO;
 
 func _physics_process(delta: float) -> void:
-	self.nav_agent.target_position = target.global_position;
-	var position: Vector3 = self.nav_agent.get_next_path_position();
-	if self.nav_agent.get_path_length() <= distance_tolerance:
-		self.direction_vec = Vector2.ZERO;
-		self._physics_process_in_range(delta);
-		return;
-	position -= self.current_position_node.global_position;
-	self.direction_vec = Vector2(position.x, position.z).normalized();
+	self.direction_vec = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	sprite.rotation = Vector3(0,0,0)
 
 func _physics_process_in_range(_delta: float) -> void:
 	pass;
